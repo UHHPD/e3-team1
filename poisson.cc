92c3bb7d6bb4afa7abf172b670c697f4fb508c44
Stephan Lachnit <stephanlachnit@debian.org>
2022-11-02 16:30:23 +0100

    Aufgabe 1
    Signed-off-by: Stephan Lachnit <stephanlachnit@debian.org>

--- poisson.cc
+++ poisson.cc
@@ -1,20 +1,35 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
 
 
 double poisson(double mu, int k) {
-    return 0;
+    return pow(mu, k) * exp(-mu) / tgamma(k+1);
 }
 
 int main() {
-    using namespace std;
-
-
-    ifstream fin("datensumme.txt");
+    std::vector<int> zaehler(11);
+    constexpr std::size_t lines {234};
+    std::ifstream fin("datensumme.txt");
     int n_i;
-    for(int i = 0 ; i < 234 ; ++i) {
+    for(int i = 0 ; i < lines ; ++i) {
         fin >> n_i;
+        if (n_i > zaehler.size()) {std::cout << "Problem, found: " << n_i << std::endl;};
+        zaehler[n_i]++;
     }
     fin.close();
+    std::ofstream fout("hist.txt");
+    for(std::size_t n = 0; n < zaehler.size(); ++n) {
+        fout << n << " " << zaehler[n] << std::endl;
+    }
+    fout.close();
+
+
+    constexpr double mean = 3.11538;
+    std::ofstream fout2("histpoi.txt");
+    for(std::size_t k = 0; k < zaehler.size(); ++k) {
+        fout2 << k << " " << lines * poisson(mean, k) << std::endl;
+    }
+    fout2.close();
 }
